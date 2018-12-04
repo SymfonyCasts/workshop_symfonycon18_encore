52b283ddabb5d1ce57561d938a84652d23a6b09e
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:05:06 +0000

    Step 49: CSS: Bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@
 const $ = require('jquery');
 require('bootstrap');
 require('jquery-tags-input/dist/jquery.tagsinput.min');
+require('bootstrap/dist/css/bootstrap.css');
 require('../css/app.css');
 
     $(document).ready(function() {
