b1b940fd158a061418e63e15badd306b1ee7e012
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:05:17 +0000

    Step 76: Code Splitting: Add the .default()

--- assets/js/login.js
+++ assets/js/login.js
@@ -20,7 +20,7 @@ import '../css/login.css';
 
             if ($emailInput.val().length >= 30) {
                 import('./Components/show_long_email_error').then(showLoginEmailError => {
-                    showLoginEmailError($emailInput);
+                    showLoginEmailError.default($emailInput);
                 });
             }
         });
