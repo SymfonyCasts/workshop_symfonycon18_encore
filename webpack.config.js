15792f765198bd759cde56daec1c64e214de405e
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:04:53 +0000

    Step 25: Install: set up entry, remove runtime

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,13 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('rep_log', './public/assets/js/RepLogApp.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
