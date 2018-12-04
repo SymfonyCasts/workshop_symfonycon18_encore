0538e2b2b8d8d6a43defdeb6221098ca0195d816
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:04:44 +0000

    Step 6: play.js: setTimeout

--- play.js
+++ play.js
@@ -2,4 +2,7 @@ const Conference = require('./conference');
 
 const sfCon = new Conference('SymfonyCon 2018');
 
-console.log(sfCon.name);
+setTimeout(function() {
+    console.log(sfCon.name);
+}, 500);
+console.log('Done!');
