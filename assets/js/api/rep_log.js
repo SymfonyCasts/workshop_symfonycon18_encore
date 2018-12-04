93e425ed9edcaaeaea6272ce27cffe46fd2eb3b0
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:05:18 +0000

    Step 78: Fetch: Add credentials

--- assets/js/api/rep_log.js
+++ assets/js/api/rep_log.js
@@ -1,7 +1,10 @@
 import Routing from '../Components/Routing';
 
 export function getRepLogs() {
-    return fetch(Routing.generate('rep_log_list'))
+    return fetch(Routing.generate('rep_log_list'), {
+        // is the new default value
+        credentials: 'same-origin'
+    })
         .then(response => {
             return response.json();
         });
