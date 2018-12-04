d3fae3187f54e2f4806a5ee1d1b3d716a74262dc
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:05:12 +0000

    Step 63: Polyfill: Add polyfill

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'jquery-tags-input/dist/jquery.tagsinput.min';
 import '../css/app.scss';
+import '@babel/polyfill';
 
     $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
