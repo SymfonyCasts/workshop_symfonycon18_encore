6206091b33f2b77f14be2d4a1c538f2a90d9bf83
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:04:48 +0000

    Step 14: Promise: Return a Promise from Promise

--- play.js
+++ play.js
@@ -7,7 +7,11 @@ sfCon.printNameLater()
         console.log('callback!');
         console.log(data);
 
-        return data * 100;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(data * 100);
+            }, 2000)
+        });
     })
     .then((data) => {
         console.log(data);
