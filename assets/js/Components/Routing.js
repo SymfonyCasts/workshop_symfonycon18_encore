0e6dc2f9ae903c3e0bc1b897987267eafb8a7523
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:05:14 +0000

    Step 69: FOSJsRoutingBundle: Load properly

--- assets/js/Components/Routing.js
+++ assets/js/Components/Routing.js
@@ -1,1 +1,6 @@
-export default global.Routing;
+const routes = require('../../../var/fos_js_routes.json');
+import Routing from '../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+
+Routing.setRoutingData(routes);
+
+export default Routing;
