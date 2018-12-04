5e5d4f540017b66e075e5b866d10f549062ad506
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:04:48 +0000

    Step 15: Webpack: Refactor Helper to module

--- public/assets/js/RepLogApp.js
+++ public/assets/js/RepLogApp.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Helper = require('./RepLogAppHelper');
+
 (function(window, $, Routing, swal) {
 
     let HelperInstances = new WeakMap();
@@ -191,37 +193,6 @@
             this.$wrapper.find('tbody').append($row);
 
             this.updateTotalWeightLifted();
-        }
-    }
-
-    class Helper {
-        constructor(repLogs) {
-            this.repLogs = repLogs;
-        }
-
-        calculateTotalWeight() {
-            return Helper._calculateWeights(
-                this.repLogs
-            );
-        }
-
-        getTotalWeightString(maxWeight = 500) {
-            let weight = this.calculateTotalWeight();
-
-            if (weight > maxWeight) {
-                weight = maxWeight + '+';
-            }
-
-            return weight + ' lbs';
-        }
-
-        static _calculateWeights(repLogs) {
-            let totalWeight = 0;
-            for (let repLog of repLogs) {
-                totalWeight += repLog.totalWeightLifted;
-            }
-
-            return totalWeight;
         }
     }
 
---
+++ public/assets/js/RepLogAppHelper.js
@@ -0,0 +1,30 @@
+module.exports = class Helper {
+    constructor(repLogs) {
+        this.repLogs = repLogs;
+    }
+
+    calculateTotalWeight() {
+        return Helper._calculateWeights(
+            this.repLogs
+        );
+    }
+
+    getTotalWeightString(maxWeight = 500) {
+        let weight = this.calculateTotalWeight();
+
+        if (weight > maxWeight) {
+            weight = maxWeight + '+';
+        }
+
+        return weight + ' lbs';
+    }
+
+    static _calculateWeights(repLogs) {
+        let totalWeight = 0;
+        for (let repLog of repLogs) {
+            totalWeight += repLog.totalWeightLifted;
+        }
+
+        return totalWeight;
+    }
+}
