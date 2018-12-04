7add214e4ab78722094936beb71091865a8d708c
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:05:07 +0000

    Step 53: Sass: Install sass deps

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
