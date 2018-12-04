642e84e780faa992785995e0bdb2b8d1083ec90a
Ryan Weaver <ryan@thatsquality.com>
2018-12-04 08:04:57 +0000

    Step 31: Multipage: Remove self-executing

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,6 +1,7 @@
 'use strict';
 
-(function(window, $) {
+const $ = require('jquery');
+
     $(document).ready(function() {
         $('.js-recommended-login').on('click', '.js-show-login', function(e) {
             e.preventDefault();
@@ -19,4 +20,3 @@
             }
         });
     });
-})(window, jQuery);
